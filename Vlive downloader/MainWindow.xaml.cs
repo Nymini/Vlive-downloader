0de9367bc3a73eeb60c1fd2adb3e61241ccba388
Nymini <z5112732@ad.unsw.edu.au>
2019-05-17 18:00:02 +1000

    Initial Commit

---
+++ Vlive downloader/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Vlive_downloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Handle POST/GET requests
+        private static readonly HttpClient client = new HttpClient();
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _urlInsert_Click(object sender, RoutedEventArgs e)
+        {
+            string url = _url.Text;
+
+            string html;
+            using (WebClient client = new WebClient())
+            {
+                //https://www.vlive.tv/video/127002?channelCode=F5F127
+                html = client.DownloadString(url);
+                
+            }
+
+            int n_match = 0;
+            string vid_id = null;
+            string key = null;
+            // Look through each line in html finding a regex match.
+            // #TODO: There are better methods than iterating through html.
+            foreach (var line in html.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                Match m = Regex.Match(line, @"video.init");
+                if (n_match == 2)
+                {
+                    key = line;
+                    break;
+                }
+                if (n_match == 1)
+                {
+                    vid_id = line;
+                    n_match++;
+                }
+                if (m.Success)
+                {
+                    n_match++;
+                }
+            }
+            handleRequest(cleanify(vid_id), cleanify(key));
+        }
+        
+        // Removes unwanted characters from key and video id
+        private string cleanify(string str)
+        {
+            string res = str;
+            res = Regex.Replace(res, "\"", "");
+            res = Regex.Replace(res, "source:", "");
+            res = Regex.Replace(res, "name:", "");
+            res = Regex.Replace(res, @"\s+", "");
+            res = Regex.Replace(res, ",", "");
+
+            return res;
+        }
+
+        private async void handleRequest(string vidId, string key)
+        {
+            string url = "http://global.apis.naver.com/rmcnmv/rmcnmv/vod_play_videoInfo.json";
+            var param = new Dictionary<string, string>
+            {
+                { "videoId" , vidId },
+                { "key" , key },
+                { "ptc" , "http" },
+                { "doct" , "json" },
+                { "cpt" , "vtt" }
+            };
+            var content = new FormUrlEncodedContent(param);
+
+            var response = await client.PostAsync(url, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            
+            var json = JsonConvert.DeserializeObject<Dictionary<String, object>>(responseString);
+            var list = JsonConvert.DeserializeObject<Dictionary<String, object>>(json["videos"].ToString());
+            var tmp = list["list"].ToString();
+
+            List<string> videoLinks = new List<string>();
+            List<string> names = new List<string>();
+            foreach (var line in tmp.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                Console.Write(line + "\n\n");
+                Match m = Regex.Match(line, @"source");
+                if (m.Success)
+                {
+                    videoLinks.Add(cleanify(line));
+                }
+                Match n = Regex.Match(line, @"name");
+                if (n.Success)
+                {
+                    names.Add(cleanify(line));
+                }
+            }
+            for (int i = 0; i < videoLinks.Count; i++)
+            {
+                _videoList.Items.Add(videoLinks[i] + " " + names[i]);
+            }
+        }
+    }
+}
