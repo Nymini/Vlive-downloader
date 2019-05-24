f1e637cb7e55d547a079fe83ad4541d2364d2340
Nymini <z5112732@ad.unsw.edu.au>
2019-05-24 23:40:47 +1000

    Cleaned up code slightly + fixed issue with titles not always retrieving

--- Vlive Downloader Material/MainWindow.xaml.cs
+++ Vlive Downloader Material/MainWindow.xaml.cs
@@ -40,6 +40,8 @@ namespace Vlive_Downloader_Material
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Initialise preferred resolution options.
             _prefRes.Items.Add("None");
             _prefRes.Items.Add("1080P");
             _prefRes.Items.Add("720P");
@@ -47,9 +49,12 @@ namespace Vlive_Downloader_Material
             _prefRes.Items.Add("360P");
             _prefRes.Items.Add("270P");
             _prefRes.Items.Add("144P");
+
+            // Initialise preferred subtitle options.
             _prefSub.Items.Add("None");
             _prefSub.Items.Add("English");
 
+            // Set preferred subtitle/resolution to what was chosen in the config file.
             _prefRes.SelectedItem = ConfigurationManager.AppSettings.Get("prefRes");
             _prefSub.SelectedItem = ConfigurationManager.AppSettings.Get("prefSub");
         }
@@ -60,6 +65,11 @@ namespace Vlive_Downloader_Material
             string url = _url.Text;
             //url = "https://www.vlive.tv/video/127002?channelCode=F5F127";
             string html;
+            int n_match = 0;
+            string vid_id = null;
+            string key = null;
+            string img = null;
+            string name = null;
 
             if (url == null || url == "")
             {
@@ -67,6 +77,8 @@ namespace Vlive_Downloader_Material
                 _url.Clear();
                 return;
             }
+
+            // Check for valid URL
             Match v = Regex.Match(url, @"vlive.tv/video/");
             if (!v.Success)
             {
@@ -79,20 +91,19 @@ namespace Vlive_Downloader_Material
             using (WebClient client = new WebClient())
             {
                 //https://www.vlive.tv/video/127002?channelCode=F5F127
-                html = client.DownloadString(url);
+                var htmlStream = client.DownloadData(url);
+                html = Encoding.UTF8.GetString(htmlStream);
 
             }
 
-            int n_match = 0;
-            string vid_id = null;
-            string key = null;
-            string img = null;
-            string name = null;
+            //System.Diagnostics.Debug.Write(html);
             // Look through each line in html finding a regex match.
             // #TODO: There are better methods than iterating through html.
             foreach (var line in html.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries))
             {
                 Match m = Regex.Match(line, @"video.init");
+                Match n = Regex.Match(line, "_video_thumb");
+                Match o = Regex.Match(line, @"og:title");
                 if (n_match == 2)
                 {
                     key = line;
@@ -106,18 +117,20 @@ namespace Vlive_Downloader_Material
                 if (m.Success && n_match == 0)
                 {
                     n_match++;
-                }
-                Match n = Regex.Match(line, "_video_thumb");
+                } 
                 if (n.Success)
                 {
-                    //We use regex for now.
                     img = getImageURL(cleanify(line));
-
+                }
+                if (o.Success)
+                {
+                    name = Get_Name(line);
                 }
             }
 
-            
-            handleRequest(cleanify(vid_id), cleanify(key), img);
+            // Create a POST request
+            handleRequest(cleanify(vid_id), cleanify(key), img, name);
+
             // Make controls visible/invisible now that we have a video
             _download.Visibility = Visibility.Visible;
             _hasContent.Visibility = Visibility.Hidden;
@@ -126,9 +139,13 @@ namespace Vlive_Downloader_Material
         }
 
         // Create a POST request to retrieve JSON content
-        private async void handleRequest(string vidId, string key, string img)
+        private async void handleRequest(string vidId, string key, string img, string backName)
         {
+            // URL to make api calls
             string url = "http://global.apis.naver.com/rmcnmv/rmcnmv/vod_play_videoInfo.json";
+            Dictionary<string, string> subLabels = new Dictionary<string, string>();
+            List<string> videoLinks = new List<string>();
+            List<string> names = new List<string>();
             var param = new Dictionary<string, string>
             {
                 { "videoId" , vidId },
@@ -138,15 +155,22 @@ namespace Vlive_Downloader_Material
                 { "cpt" , "vtt" }
             };
             var content = new FormUrlEncodedContent(param);
-
             var response = await client.PostAsync(url, content);
-
             var responseString = await response.Content.ReadAsStringAsync();
-            System.Diagnostics.Debug.Write(responseString);
+
+            //System.Diagnostics.Debug.Write(responseString);
             // Hacky way to retrieve JSON content
             var json = JsonConvert.DeserializeObject<Dictionary<String, object>>(responseString);
             var list = JsonConvert.DeserializeObject<Dictionary<String, object>>(json["videos"].ToString());
             var meta = JsonConvert.DeserializeObject<Dictionary<String, object>>(json["meta"].ToString());
+            string name = meta["subject"].ToString();
+            // If name not in json, we use a backup that we scraped
+            if (name == "")
+            {
+                name = backName;
+            }
+
+            // Attempt to retrieve subs if there are any.
             Dictionary<string, object> sub;
             try
             {
@@ -155,55 +179,57 @@ namespace Vlive_Downloader_Material
             {
                 sub = null;
             }
-            string name = meta["subject"].ToString();
+            
             var tmp = list["list"].ToString();
-
-            Dictionary<string, string> subLabels = new Dictionary<string, string>();
-            List<string> videoLinks = new List<string>();
-            List<string> names = new List<string>();
 
             if (sub != null)
             {
                 subLabels = retrieveSubs(sub["list"].ToString());
             }
+
+            // Look through and find the video URL + the Resolution
             foreach (var line in tmp.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries))
             {
 
                 Match m = Regex.Match(line, @"source");
+                Match n = Regex.Match(line, @"name");
                 if (m.Success)
                 {
                     videoLinks.Add(cleanify(line));
                 }
-                Match n = Regex.Match(line, @"name");
                 if (n.Success)
                 {
                     names.Add(cleanify(line));
                 }
             }
+
+            // Turn two lists to single dictionary.
+            // TODO: Combine above step and this together.
             Dictionary<string, string> dicNames = new Dictionary<string, string>();
             for (int i = 0; i < names.Count; i++)
             {
                 dicNames.Add(names[i], videoLinks[i]);
             }
 
+            // Finally add object to the list
             createObject(dicNames, img, name, subLabels);
         }
 
-        private string getImageURL(string r)
-        {
-            string cleaned = Regex.Replace(r, "<imgsrc=", "");
-            cleaned = Regex.Replace(cleaned, @"class=.*", "");
-            return cleaned;
-        }
-
+        
         private async void createObject(Dictionary<string, string> res, string img, string name, Dictionary<string, string> labels)
         {
-            _snackbar.MessageQueue.Enqueue("Added video to queue", "Nice", () => Foo());
             string colour = Colourize();
-            VideoItem v = new VideoItem(new BitmapImage(new Uri(img)), name, colour, res, labels );
-            v.Margin = new Thickness(5, 10, 5, 0);
+            var bc = new BrushConverter();
+
+            // Grid that will hold both our remove button and the video item object
             Grid g = new Grid();
 
+            // Create VideoItem object using given parameters.
+            VideoItem v = new VideoItem(new BitmapImage(new Uri(img)), name, colour, res, labels );
+            v.Margin = new Thickness(5, 10, 5, 0);
+            
+
+            // Set video resolution + subtitle to preferred option
             if (_prefRes.SelectedItem.ToString() != "None")
             {
                 v.Set_Res(_prefRes.SelectedItem.ToString());
@@ -212,13 +238,14 @@ namespace Vlive_Downloader_Material
             {
                 v.Set_Sub(_prefSub.SelectedItem.ToString());
             }
+
+            // If no subtitles were found, hide the subtitle label
             if (labels.Count == 0)
             {
                 v._subLabel.Visibility = Visibility.Hidden;
             }
-            g.Children.Add(v);
-            var bc = new BrushConverter();
-
+            
+          
             // Create a remove button to add to grid
             Button b = new Button();
             b.Content = "Remove";
@@ -230,25 +257,34 @@ namespace Vlive_Downloader_Material
             b.Background = (Brush)bc.ConvertFrom("#FF" + colour);
             b.BorderBrush = (Brush)bc.ConvertFrom("#FF" + colour);
             b.Click += Remove;
+
+            // Add children to our grid
+            g.Children.Add(v);
             g.Children.Add(b);
             g.Margin = new Thickness(20, 10, 20, 10);
+
+            // Add the grid object to our list
             _videoList.Items.Add(g);
+            _snackbar.MessageQueue.Enqueue("Added video to queue", "Nice", () => Foo());
             _url.Clear();
         }
 
         private async void Download(object sender, RoutedEventArgs e)
         {
 
+            // Precheck to see if all videos have a chosen resolution
             foreach (Grid g in _videoList.Items)
             {
                 VideoItem v = g.Children[0] as VideoItem;
                 if (v.Get_Res() == null)
                 {
-                    //System.Diagnostics.Debug.Write("oh no\n");
                     _snackbar.MessageQueue.Enqueue("One or more videos does not have a chosen resolution");
                     return;
                 }
             }
+
+            // Hide these buttons to prevent unwanted effects
+            // TODO: Allow users to add files while downloading
             _download.Visibility = Visibility.Hidden;
             _add.Visibility = Visibility.Hidden;
             downloadFile();
@@ -257,8 +293,16 @@ namespace Vlive_Downloader_Material
 
         private void downloadFile()
         {
+            // Retrieve our VideoItem for the current file
+            Grid g = _videoList.Items[curr] as Grid;
+            VideoItem v = g.Children[0] as VideoItem;
 
-            System.Diagnostics.Debug.Write(curr.ToString() + " " + _videoList.Items.Count.ToString());
+            string name = v._title.Text;
+            string dlLink = v.DLLink();
+            string locale = v.SubLink();
+            string res = v.Get_Res();
+
+            // No more files to download
             if (curr >= _videoList.Items.Count)
             {
                 _fin.IsActive = true;
@@ -268,16 +312,10 @@ namespace Vlive_Downloader_Material
                 m.ActionClick += Remove_All;
                 _fin.Message = m;
                 _add.Visibility = Visibility.Hidden;
-  
                 return;
             }
-            Grid g = _videoList.Items[curr] as Grid;
-            VideoItem v = g.Children[0] as VideoItem;
 
-            string name = v._title.Text;
-            string dlLink = v.DLLink();
-            string locale = v.SubLink();
-            string res = v.Get_Res();
+            // No subtitle chosen, so no download
             if (locale != null)
             {
                 using (WebClient client = new WebClient())
@@ -286,6 +324,7 @@ namespace Vlive_Downloader_Material
                 }
             }
 
+            // Download file
             using (WebClient client = new WebClient())
             {
                 System.Diagnostics.Debug.Write(dlLink + "\n" + name + "\n" + res);
@@ -311,7 +350,7 @@ namespace Vlive_Downloader_Material
 
         private Dictionary<string, string> retrieveSubs(string src)
         {
-
+            // Look through all subtitles and add it to a dict
             List<string> subLinks = new List<string>();
             List<string> label = new List<string>();
             foreach (var line in src.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries))
@@ -338,7 +377,6 @@ namespace Vlive_Downloader_Material
                     if (!seen)
                     {
                         seenSubs.Add(tmp);
-                        System.Diagnostics.Debug.Write(tmp + "\n");
                     }
                 }
             }
@@ -351,6 +389,14 @@ namespace Vlive_Downloader_Material
                 }
             }
             return dic;
+        }
+
+        // Regexs the URL of the video thumbnail
+        private string getImageURL(string r)
+        {
+            string cleaned = Regex.Replace(r, "<imgsrc=", "");
+            cleaned = Regex.Replace(cleaned, @"class=.*", "");
+            return cleaned;
         }
 
         // Removes unwanted characters from key and video id
@@ -367,6 +413,7 @@ namespace Vlive_Downloader_Material
             return res;
         }
 
+        // Pretty colours
         private string Colourize()
         {
             if (colours.Count == 0)
@@ -387,6 +434,7 @@ namespace Vlive_Downloader_Material
             return colour;
         }
 
+        // Remove the parent of the clicked button.
         private async void Remove(object sender, RoutedEventArgs e)
         {
             Grid g = ((e.Source as Button).Parent as Grid);
@@ -405,11 +453,13 @@ namespace Vlive_Downloader_Material
             }
         }
 
+        // Stub function for our snackbar. Not needed? Idk
         public void Foo()
         {
 
         }
 
+        // Clear list and reset everything
         private async void Remove_All(object sender, RoutedEventArgs e)
         {
             _videoList.Items.Clear();
@@ -444,6 +494,16 @@ namespace Vlive_Downloader_Material
 
             return string.Join(" ", filename.Split(System.IO.Path.GetInvalidFileNameChars()));
 
+        }
+
+        private string Get_Name(string line)
+        {
+            line = Regex.Replace(line, @".*content=", "");
+            line = Regex.Replace(line, "/>", "");
+            line = Regex.Replace(line, "\"", "");
+            System.Diagnostics.Debug.Write(line);
+    
+            return line;
         }
     }
 }
